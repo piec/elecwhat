f7a2096bb9698af64ad13ee5975ee1f13305c056
Pierre Carru <pierre@carru.fr>
2025-03-05 18:37:55 +0100

    new shortcuts

--- src-web/injected.js
+++ src-web/injected.js
@@ -37,5 +37,60 @@ async function hijack() {
   hijackNotif(prefix);
 }
 
+const addedKeys = [
+  {
+    key: "ArrowDown",
+    ctrlKey: false,
+    shiftKey: false,
+    altKey: true,
+    handler: () => {
+      importDefault("WAWebKeyboardRun")("GO_TO_NEXT_CHAT");
+    },
+  },
+  {
+    key: "ArrowUp",
+    ctrlKey: false,
+    shiftKey: false,
+    altKey: true,
+    handler: () => {
+      importDefault("WAWebKeyboardRun")("GO_TO_PREV_CHAT");
+    },
+  },
+  {
+    key: "Tab",
+    ctrlKey: true,
+    shiftKey: false,
+    altKey: false,
+    handler: () => {
+      importDefault("WAWebKeyboardRun")("GO_TO_NEXT_CHAT");
+    },
+  },
+  {
+    key: "Tab",
+    ctrlKey: true,
+    shiftKey: true,
+    altKey: false,
+    handler: () => {
+      importDefault("WAWebKeyboardRun")("GO_TO_PREV_CHAT");
+    },
+  },
+  {
+    key: "u",
+    ctrlKey: true,
+    shiftKey: false,
+    altKey: false,
+    handler: () => {
+      importDefault("WAWebKeyboardRun")("TOGGLE_UNREAD");
+    },
+  },
+];
+
+addEventListener("keydown", (e) => {
+  for (const key of addedKeys) {
+    if (e.key == key.key && e.ctrlKey == key.ctrlKey && e.shiftKey == key.shiftKey && e.altKey == key.altKey) {
+      key.handler(e);
+    }
+  }
+});
 
 void hijack();
