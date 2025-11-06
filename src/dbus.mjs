dd730c3cdc08adbb61466f54617be51f4d0c1a0d
Pierre Carru <pierre@carru.fr>
2025-11-06 22:00:23 +0100

    fix badge when run in snap

--- src/dbus.mjs
+++ src/dbus.mjs
@@ -1,5 +1,6 @@
 import dbus from "@homebridge/dbus-native";
 import * as os from "node:os";
+import * as path from "node:path";
 import { app } from "electron";
 import { toggleVisibility, windowShow } from "./util.mjs";
 
@@ -76,6 +77,8 @@ export class Dbus {
     // https://wiki.ubuntu.com/Unity/LauncherAPI
     // gnome dash-to-dock: https://github.com/micheleg/dash-to-dock/blob/b96a003bec758fc35efb0f4e5134030a921c418b/launcherAPI.js#L14-L21
     // kde plasma: https://github.com/KDE/plasma-desktop/commit/e284e9dc17051f22d05985e218fa44ddaba78de5
+    const hint = process.env.BAMF_DESKTOP_FILE_HINT;
+    const desktopFile = hint ? path.basename(hint) : "elecwhat.desktop";
     this.sessionBus?.connection.message({
       type: dbus.messageType.signal,
       serial: ++this.serialCounter,
@@ -84,7 +87,7 @@ export class Dbus {
       member: "Update",
       signature: "sa{sv}",
       body: [
-        "application://elecwhat.desktop",
+        `application://${desktopFile}`,
         [
           ["count", ["x", number]],
           ["count-visible", ["b", number !== 0]],
