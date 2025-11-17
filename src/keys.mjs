3c93a5df0bd42d20e4bc680b1dae181dd21458b5
Pierre Carru <pierre@carru.fr>
2025-11-17 12:21:20 +0100

    add new default binding alt+l: toggle window + lock
    Fixes #37

--- src/keys.mjs
+++ src/keys.mjs
@@ -2,7 +2,7 @@ const nthChatBindings = Object.fromEntries(
   ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"].flatMap((key, index) => [
     [`A ${key}`, { action: "OPEN_NTH_CHAT", chatIndex: index }],
     [`C ${key}`, { action: "OPEN_NTH_CHAT", chatIndex: index }],
-  ])
+  ]),
 );
 
 export const defaultKeys = {
@@ -24,5 +24,14 @@ export const defaultKeys = {
   "C ArrowUp": {
     whatsappAction: "EDIT_LAST_MESSAGE",
   },
+  "A l": [
+    {
+      action: "IPC",
+      method: "windowToggle",
+    },
+    {
+      whatsappAction: "LOCK_SCREEN",
+    },
+  ],
   ...nthChatBindings,
 };
